7634a68f302919203879e032ebf0107ee66a63cd
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2018-10-03 15:23:56 +0900

    initial commit

---
+++ init.el
@@ -0,0 +1,277 @@
+;; ==== ==== ==== ==== GENRAL SETTINGS ==== ==== ==== ====
+;; ---- ---- garbage collection ---- ----
+(setq gc-cons-threshold (* 20 gc-cons-threshold))
+
+;; ---- ---- coding system ---- ----
+(prefer-coding-system 'utf-8)
+(setq coding-system-for-read 'utf-8)
+(setq coding-system-for-write 'utf-8)
+(set-language-environment "Japanese")
+
+;(setq ns-pop-up-frames nil)
+(setq-default indent-tabs-mode nil)
+(setq load-prefer-newer t)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; ==== ==== ==== ==== FILETYPE ASSOCIATION ==== ==== ==== ====
+;; ---- ---- LaTeX ---- ----
+(add-to-list 'auto-mode-alist '("\\.tex$" . gfn-latex-mode))
+(add-to-list 'auto-mode-alist '("\\.ltx$" . gfn-latex-mode))
+(add-to-list 'auto-mode-alist '("\\.cls$" . gfn-latex-mode))
+(add-to-list 'auto-mode-alist '("\\.sty$" . gfn-latex-mode))
+(add-to-list 'auto-mode-alist '("\\.clo$" . gfn-latex-mode))
+(add-to-list 'auto-mode-alist '("\\.bbl$" . gfn-latex-mode))
+
+;; ---- ---- OCaml ---- ----
+(add-to-list 'auto-mode-alist '("\\.ml$" . tuareg-mode))
+(add-to-list 'auto-mode-alist '("\\.mli$" . tuareg-mode))
+(add-to-list 'auto-mode-alist '("\\.mll$" . tuareg-mode))
+(add-to-list 'auto-mode-alist '("\\.mly$" . tuareg-mode))
+
+;; ---- ---- Coq ---- ----
+(add-to-list 'auto-mode-alist '("\\.v$" . coq-mode))
+
+;; ---- ---- Markdown ---- ----
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+;; ---- ---- Macrodown ---- ----
+(add-to-list 'auto-mode-alist '("\\.mcrd$" . mcrd-mode))
+(add-to-list 'auto-mode-alist '("\\.mcrdh$" . mcrd-mode))
+(add-to-list 'auto-mode-alist '("\\.mcrds$" . mcrd-mode))
+
+;; ---- ---- SATySFi ---- ----
+(add-to-list 'auto-mode-alist '("\\.saty$" . satysfi-mode))
+(add-to-list 'auto-mode-alist '("\\.satyh$" . satysfi-mode))
+(add-to-list 'auto-mode-alist '("\\.satyg$" . satysfi-mode))
+
+;; ==== ==== ==== ==== USER INTERFACE ==== ==== ==== ====
+;; ---- ---- hide tool bar and menu bar ---- ----
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; ---- ---- font settings ---- ----
+(set-face-attribute 'default nil :family "Monaco" :height 130)
+;(set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Meiryo UI"))
+;(setq face-font-rescale-alist '(("Meiryo UI" . 1.08)))
+
+;;色
+;;(set-face-background 'hl-line "darkolivegreen")
+
+;; ---- ---- highlighting current line ---- ----
+(global-hl-line-mode 1)
+
+;; ---- ---- show balanced paren ---- ----
+(show-paren-mode 1)
+
+;;eldocを使う
+;;(add-hook emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+
+;; ---- ---- display line and column number ---- ----
+(line-number-mode 1)
+(column-number-mode 1)
+
+;; ==== ==== general functions ==== ====
+
+(defun gfn-insert-paren-pair-scheme (opn cls)
+  (interactive)
+  (cond ((use-region-p)
+         (let ((rb (region-beginning)))
+           (let ((re (region-end)))
+             (progn
+               (goto-char rb)
+               (insert opn)
+               (goto-char (1+ re))
+               (insert cls)
+               (forward-char -1)))))
+        (t
+         (progn
+           (insert opn)
+           (insert cls)
+           (forward-char -1)))))
+
+(defun gfn-insert-paren-pair ()
+  (interactive)
+  (gfn-insert-paren-pair-scheme "(" ")"))
+
+(defun gfn-insert-square-bracket-pair ()
+  (interactive)
+  (gfn-insert-paren-pair-scheme "[" "]"))
+
+(defun gfn-insert-brace-pair ()
+  (interactive)
+  (gfn-insert-paren-pair-scheme "{" "}"))
+
+;; ==== ==== ==== ==== KEY BIND ==== ==== ==== ====
+;(global-set-key [M-kanji] 'ignore)
+;(global-set-key [kanji] 'ignore)
+;; ---- ---- common ---- ----
+(define-key global-map (kbd "C-c s") 'query-replace)
+(define-key global-map (kbd "C-c a") 'beginning-of-buffer)
+(define-key global-map (kbd "C-c e") 'end-of-buffer)
+(define-key global-map (kbd "C-c t") 'shell)
+(define-key global-map (kbd "C-h") 'delete-backward-char)
+;(define-key global-map (kbd "M-n") 'scroll-up-command)
+;(define-key global-map (kbd "M-p") 'scroll-down-command)
+(define-key global-map (kbd "(") 'gfn-insert-paren-pair)
+(define-key global-map (kbd "[") 'gfn-insert-square-bracket-pair)
+(define-key global-map (kbd "{") 'gfn-insert-brace-pair)
+;; ---- ---- original ---- ----
+;(define-key global-map (kbd "C-c m") 'gfn-macro)
+;(define-key global-map (kbd "C-c l") 'gfn-insert-line)
+;; ---- ---- helm ---- ----
+(define-key global-map (kbd "C-x C-b") 'helm-buffers-list)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+;; ---- ---- recentf ---- ----
+(define-key global-map (kbd "C-c f") 'recentf-open-files)
+;; ---- ---- tabbar ---- ----
+(define-key global-map (kbd "M-<right>") 'tabbar-forward-tab)
+(define-key global-map (kbd "M-<left>") 'tabbar-backward-tab)
+(define-key global-map (kbd "C->") 'tabbar-forward-tab)
+(define-key global-map (kbd "C-<") 'tabbar-backward-tab)
+;; ---- ---- open-junk-file ---- ----
+(define-key global-map (kbd "C-x j") 'open-junk-file)
+;; ---- ---- undo-tree ---- ----
+(define-key global-map (kbd "M-/") 'undo-tree-redo)
+(define-key global-map (kbd "C-z") 'undo-tree-undo)
+;; ---- ---- point-undo ---- ----
+(define-key global-map (kbd "<f7>") 'point-undo)
+(define-key global-map (kbd "S-<f7>") 'point-redo)
+
+;; ==== ==== ==== ==== DISTRIBUTED PACKAGES ==== ==== ==== ====
+;; ---- ---- package ---- ----
+(require 'package)
+(add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade"    . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/") t)
+
+(package-initialize)
+
+;; ---- ---- exec-path-from-shell ---- ----
+(require 'exec-path-from-shell)
+(exec-path-from-shell-initialize)
+;; ---- ---- f ---- ----
+;(require 'f)
+
+;; ---- ---- magit ---- ----
+;(require 'magit)
+
+;; ---- ---- unicode-escape ---- ----
+;(require 'unicode-escape)
+
+;; ---- ---- save-place ---- ----
+;(setq-default save-place t)
+;(require 'save-place)
+
+;; ---- ---- helm ---- ----
+(require 'helm-config)
+(helm-mode 1)
+
+;; ---- ---- tabbar ---- ----
+(require 'tabbar)
+(tabbar-mode 1)
+
+;; ---- ---- open-junk-file ---- ----
+(require 'open-junk-file)
+(setq open-junk-file-format "~/junk/%Y/%m/%Y-%m-%d-%H%M%S.")
+
+;; ---- ---- tuareg ---- ----
+(require 'tuareg)
+(setq tuareg-use-smie nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(package-selected-packages
+   (quote
+    (sml-mode flymake-cursor point-undo htmlize markdown-mode exec-path-from-shell undo-tree tuareg tabbar restart-emacs recentf-ext paredit open-junk-file helm auto-complete auto-async-byte-compile)))
+ '(tuareg-match-clause-indent 2))
+
+;; ---- ---- merlin ---- ----
+(let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
+ (when (and opam-share (file-directory-p opam-share))
+  (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+  (autoload 'merlin-mode "merlin" nil t nil)
+  (add-hook 'tuareg-mode-hook 'merlin-mode t)
+  (add-hook 'caml-mode-hook 'merlin-mode t)))
+(setq merlin-ac-setup t)
+
+;; ---- ---- coq ---- ----
+(setq auto-mode-alist (cons '("\\.v$" . coq-mode) auto-mode-alist))
+(autoload 'coq-mode "gallina" "Major mode for editing Coq vernacular." t)
+(autoload 'run-coq "inferior-coq" "Run an inferior Coq process." t)
+(autoload 'run-coq-other-window "inferior-coq" "Run an inferior Coq process in a new window." t)
+(autoload 'run-coq-other-frame "inferior-coq" "Run an inferior Coq process in a new frame." t)
+(load-file "/usr/local/share/emacs/site-lisp/proof-general/generic/proof-site.el")
+
+;; ---- ---- install-elisp ---- ----
+;(require 'install-elisp)
+
+;; ---- ---- restart-emacs ---- ----
+(require 'restart-emacs)
+
+;; ---- ---- auto-async-byte-compile ---- ----
+(require 'auto-async-byte-compile)
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+
+;; ---- ---- undo-tree ---- ----
+(require 'undo-tree)
+(global-undo-tree-mode t)
+
+;; ---- ---- paredit ---- ----
+(require 'paredit)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
+
+;; ---- ---- auto-complete ---- ----
+(require 'auto-complete)
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
+
+;; ---- ---- recentf ---- ----
+(setq recentf-max-saved-items 2000)
+(require 'recentf-ext)
+
+;; ---- ---- autoinsert ---- ----
+(auto-insert-mode)
+(setq-default auto-insert-directory "~/.emacs.d/auto-insert/")
+(define-auto-insert "\\.tex$" "latex-template.tex")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; ---- ---- point-undo ---- ----
+(require 'point-undo)
+
+;; ---- ---- flymake / flymake-cursor ---- ----
+(require 'flymake)
+(require 'flymake-cursor)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (flymake-mode t)
+            (flymake-cursor-mode t)
+            (setq flymake-check-was-interrupted t)
+            (local-set-key (kbd "C-c C-v") 'flymake-start-syntax-check)
+            (local-set-key (kbd "C-c C-p") 'flymake-goto-prev-error)
+            (local-set-key (kbd "C-c C-n") 'flymake-goto-next-error)
+            (local-set-key (kbd "(") 'gfn-insert-paren-pair)
+            (local-set-key (kbd "[") 'gfn-insert-square-bracket-pair)
+            (local-set-key (kbd "{") 'gfn-insert-brace-pair)))
+
+;; ==== ==== ==== ==== MY PACKAGES ==== ==== ==== ====
+(require 'gfn-latex)
+(require 'open-group)
+(require 'mcrd)
+(require 'satysfi)
